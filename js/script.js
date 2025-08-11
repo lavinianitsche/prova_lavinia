cd04518cb44b91713de06a4288c1d638aed43f63
JavaliRobusto <vitor.bessa@escola.pr.gov.br>
2025-08-11 08:31:48 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+for()
+function cadastrar(){
+let nome = document.getElementById("nome").value;
+console.log(nome);
+
+var telefone = document.getElementById("telefone").value;
+console.log(telefone);
+var resultado = telefone.substring(0, 2);
+
+alert(resultado);
+
+}
